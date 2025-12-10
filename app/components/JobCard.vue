e34ef3fe9c8e6126bd002d15998440c2defd5368
orellana-ad <104051186+orellanaoa@users.noreply.github.com>
2025-12-09 22:48:39 -0800

    Update de opportunities page

--- app/components/JobCard.vue
+++ app/components/JobCard.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="bg-[#E6EAEF] text-white rounded-lg shadow-md p-6 w-full max-w-md">
-    <div class="flex items-center justify-between mb-2">
-      <h2 class="text-lg font-bold text-[#051E51]">{{ company }}</h2>
-      <span class="text-sm text-gray-400">{{ date }}</span>
+    <!-- Encabezado con imagen, nombre, título y fecha -->
+    <div class="flex items-start justify-between mb-4">
+      <!-- Imagen + texto -->
+      <div class="flex items-center gap-4">
+        <img
+          :src="image"
+          alt="Logo empresa"
+          class="w-12 h-12 rounded-full object-cover bg-white"
+        />
+        <div>
+          <h2 class="text-lg font-bold text-[#051E51]">{{ company }}</h2>
+          <h3 class="text-indigo-400 text-md font-medium">{{ title }}</h3>
+        </div>
+      </div>
+
+      <!-- Fecha -->
+      <span class="text-sm text-[#45C567] whitespace-nowrap">{{ date }}</span>
     </div>
-    <h3 class="text-indigo-400 text-md font-medium mb-2">{{ title }}</h3>
+
+    <!-- Descripción -->
     <p class="text-sm text-[#051E51] line-clamp-3">{{ description }}</p>
   </div>
 </template>
@@ -15,12 +30,14 @@ defineProps({
   date: String,
   title: String,
   description: String,
+  image: String,
 });
 </script>
 
 <style scoped>
 .line-clamp-3 {
   display: -webkit-box;
+  -webkit-line-clamp: 3;
   -webkit-box-orient: vertical;
   overflow: hidden;
 }
---
+++ app/pages/cvPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full">
+    <h5 class="font-bold text-2xl text-[#3459F4]">Curriculum Vitae</h5>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "main",
+});
+</script>
+<style scoped></style>
